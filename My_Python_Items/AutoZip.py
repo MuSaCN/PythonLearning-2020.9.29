19d5a6abd2367bb88f3954205e8e42ebf7395114
MuSaCN <435116098@qq.com>
2019-09-25 20:35:23 +0800

    update

--- My_Python_Items/AutoZip.py
+++ My_Python_Items/AutoZip.py
@@ -1,19 +1,24 @@
 # Author:Zhang Yuan
 import MyPackage
-
-
 __mypath__ = MyPackage.MyClass_Path.MyClass_Path()  #路径类
 myfile = MyPackage.MyClass_File.MyClass_File()  #文件操作类
 
+
+# ------MyPackage备份系列-----------------------------
 MyPackage_PathList = __mypath__.GetMyPackagePath()
 #MyPackage_PathList = ["C:\\Users\\i2011\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages\\MyPackage"]
-
 # ---备份到桌面
 myfile.ZipDir(MyPackage_PathList[0], zipPath="Desktop" , zipName=None, autoName=True)
-
 # ---备份到OneDrive的Work-Python备份文件夹
 ZipPath = __mypath__.GetOneDrivePath() + "\\Work-Python备份"
 myfile.ZipDir(MyPackage_PathList[0], zipPath=ZipPath , zipName=None, autoName=True)
 
+# ------PycharmProjects备份系列-----------------------
+ProjectsPath = "C:\\Users\\i2011\\PycharmProjects"
+# ---忽略.git文件夹，备份到桌面
+myfile.ZipDir(ProjectsPath, zipPath="Desktop", zipName=None, autoName=True, ignoreFolder=".git")
+# ---忽略.git文件夹，备份到OneDrive的Work-Python备份文件夹
+ZipPath = __mypath__.GetOneDrivePath() + "\\Work-Python备份"
+myfile.ZipDir(ProjectsPath, zipPath=ZipPath , zipName=None, autoName=True, ignoreFolder=".git")
 
 
