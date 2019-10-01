1dd4aa96f3e59c41d047bdabeb53ec2770f267a6
MuSaCN <435116098@qq.com>
2019-10-01 10:51:11 +0800

    update

--- My_Python_Items/AutoPythonZip.py
+++ My_Python_Items/AutoPythonZip.py
@@ -11,8 +11,8 @@ MyPackage_PathList = __mypath__.GetMyPackagePath() # 需压缩的目录
 OneDrive_MyPackage = myfile.ZipDir(MyPackage_PathList[0], zipPath=__mypath__.GetOneDrivePath() + "\\Work-Python备份" , zipName=None, autoName=True)
 print("......MyPackage压缩文件保存完成，{}......".format(OneDrive_MyPackage))
 # 备份到桌面(用于上传)
-Desktop_MyPackage = myfile.ZipDir(MyPackage_PathList[0], zipPath="Desktop", zipName=None, autoName=True)
-print("......MyPackage压缩文件保存完成，{}......".format(Desktop_MyPackage))
+# Desktop_MyPackage = myfile.ZipDir(MyPackage_PathList[0], zipPath="Desktop", zipName=None, autoName=True)
+# print("......MyPackage压缩文件保存完成，{}......".format(Desktop_MyPackage))
 
 
 # ------PycharmProjects备份系列
@@ -22,16 +22,19 @@ ProjectsPath = "C:\\Users\\i2011\\PycharmProjects"
 OneDrive_PycharmProjects = myfile.ZipDir(ProjectsPath, zipPath=__mypath__.GetOneDrivePath() + "\\Work-Python备份" , zipName=None, autoName=True, ignoreFolder=".git")
 print("......PycharmProjects压缩文件保存完成，{}......".format(OneDrive_PycharmProjects))
 # 忽略.git文件夹，备份到桌面(用于上传)
-Desktop_PycharmProjects = myfile.ZipDir(ProjectsPath, zipPath="Desktop" , zipName=None, autoName=True, ignoreFolder=".git")
-print("......PycharmProjects压缩文件保存完成，{}......".format(Desktop_PycharmProjects))
+# Desktop_PycharmProjects = myfile.ZipDir(ProjectsPath, zipPath="Desktop" , zipName=None, autoName=True, ignoreFolder=".git")
+# print("......PycharmProjects压缩文件保存完成，{}......".format(Desktop_PycharmProjects))
 
 
 # ------上传到Baidu网盘------------------
 '''先要授权 在Terminal中输入 bypy info ，将会出现一个提示，访问一个授权网址，在网址中输入用户名和密码，并把生成的授权码复制到命令行中。按照提示完成授权，完成了授权Python代码才能和你的百度云盘进行通信。'''
 print("------开始上传压缩文件到Baidu云盘------")
 locallist = []
-locallist.append(Desktop_MyPackage)
-locallist.append(Desktop_PycharmProjects)
+# locallist.append(Desktop_MyPackage)
+# locallist.append(Desktop_PycharmProjects)
+locallist.append(OneDrive_MyPackage)
+locallist.append(OneDrive_PycharmProjects)
+
 # ---引入类和设定网盘目录
 myBaidu= MyPackage.MyClass_WebCrawler.MyClass_BaiduPan()      #百度网盘交互类
 remotePath = "\\MyPythonBackups\\"
