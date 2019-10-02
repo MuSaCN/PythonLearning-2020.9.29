fa5fdce4b4876cc6c6c61a6685895ee3c2529f4a
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-10-02 13:17:08 +0800

    update

--- 量化投资以Python为工具/chapter26.py
+++ 量化投资以Python为工具/chapter26.py
@@ -60,135 +60,12 @@ model = myDA.PairTrading(pairf,pairt,isPrice=True,method="Cointegration",width=1
 
 
 
-
-import re
 import pandas as pd
 import numpy as np
 from arch.unitroot import ADF
 import statsmodels.api as sm
-from statsmodels.tsa.stattools import adfuller
 
 
-class PairTrading:
-    def SSD(self, priceX, priceY):
-        if priceX is None or priceY is None:
-            print('缺少价格序列.')
-        returnX = (priceX - priceX.shift(1)) / priceX.shift(1)[1:]
-        returnY = (priceY - priceY.shift(1)) / priceY.shift(1)[1:]
-        standardX = (returnX + 1).cumprod()
-        standardY = (returnY + 1).cumprod()
-        SSD = np.sum((standardY - standardX) ** 2)
-        return (SSD)
-
-    def SSDSpread(self, priceX, priceY):
-        if priceX is None or priceY is None:
-            print('缺少价格序列.')
-        priceX = np.log(priceX)
-        priceY = np.log(priceY)
-        retx = priceX.diff()[1:]
-        rety = priceY.diff()[1:]
-        standardX = (1 + retx).cumprod()
-        standardY = (1 + rety).cumprod()
-        spread = standardY - standardX
-        return (spread)
-
-    def cointegration(self, priceX, priceY):
-        if priceX is None or priceY is None:
-            print('缺少价格序列.')
-        priceX = np.log(priceX)
-        priceY = np.log(priceY)
-        results = sm.OLS(priceY, sm.add_constant(priceX)).fit()
-        resid = results.resid
-        adfSpread = ADF(resid)
-        if adfSpread.pvalue >= 0.05:
-            print('''交易价格不具有协整关系.
-            P-value of ADF test: %f
-            Coefficients of regression:
-            Intercept: %f
-            Beta: %f
-             ''' % (adfSpread.pvalue, results.params[0], results.params[1]))
-            return (None)
-        else:
-            print('''交易价格具有协整关系.
-            P-value of ADF test: %f
-            Coefficients of regression:
-            Intercept: %f
-            Beta: %f
-             ''' % (adfSpread.pvalue, results.params[0], results.params[1]))
-            return (results.params[0], results.params[1])
-
-    def CointegrationSpread(self, priceX, priceY, formPeriod, tradePeriod):
-        if priceX is None or priceY is None:
-            print('缺少价格序列.')
-        if not (re.fullmatch('\d{4}-\d{2}-\d{2}:\d{4}-\d{2}-\d{2}', formPeriod)
-                or re.fullmatch('\d{4}-\d{2}-\d{2}:\d{4}-\d{2}-\d{2}', tradePeriod)):
-            print('形成期或交易期格式错误.')
-        formX = priceX[formPeriod.split(':')[0]:formPeriod.split(':')[1]]
-        formY = priceY[formPeriod.split(':')[0]:formPeriod.split(':')[1]]
-        coefficients = self.cointegration(formX, formY)
-        if coefficients is None:
-            print('未形成协整关系,无法配对.')
-        else:
-            spread = (np.log(priceY[tradePeriod.split(':')[0]:tradePeriod.split(':')[1]])
-                      - coefficients[0] - coefficients[1] * np.log(
-                        priceX[tradePeriod.split(':')[0]:tradePeriod.split(':')[1]]))
-            return (spread)
-
-    def calBound(self, priceX, priceY, method, formPeriod, width=1.5):
-        if not (re.fullmatch('\d{4}-\d{2}-\d{2}:\d{4}-\d{2}-\d{2}', formPeriod)
-                or re.fullmatch('\d{4}-\d{2}-\d{2}:\d{4}-\d{2}-\d{2}', tradePeriod)):
-            print('形成期或交易期格式错误.')
-        if method == 'SSD':
-            spread = self.SSDSpread(priceX[formPeriod.split(':')[0]:formPeriod.split(':')[1]],
-                                    priceY[formPeriod.split(':')[0]:formPeriod.split(':')[1]])
-            mu = np.mean(spread)
-            sd = np.std(spread)
-            UpperBound = mu + width * sd
-            LowerBound = mu - width * sd
-            return (UpperBound, LowerBound)
-        elif method == 'Cointegration':
-            spread = self.CointegrationSpread(priceX, priceY, formPeriod, formPeriod)
-            mu = np.mean(spread)
-            sd = np.std(spread)
-            UpperBound = mu + width * sd
-            LowerBound = mu - width * sd
-            return (UpperBound, LowerBound)
-        else:
-            print('不存在该方法. 请选择"SSD"或是"Cointegration".')
-
-
-sh = pd.read_csv('sh50p.csv', index_col='Trddt')
-sh.index = pd.to_datetime(sh.index)
-
-formPeriod = '2014-01-01:2015-01-01'
-tradePeriod = '2015-01-01:2015-06-30'
-
-priceA = sh['601988']
-priceB = sh['600000']
-priceAf = priceA[formPeriod.split(':')[0]:formPeriod.split(':')[1]]
-priceBf = priceB[formPeriod.split(':')[0]:formPeriod.split(':')[1]]
-priceAt = priceA[tradePeriod.split(':')[0]:tradePeriod.split(':')[1]]
-priceBt = priceB[tradePeriod.split(':')[0]:tradePeriod.split(':')[1]]
-
-pt = PairTrading()
-SSD = pt.SSD(priceAf, priceBf)
-SSD
-
-SSDspread = pt.SSDSpread(priceAf, priceBf)
-SSDspread.describe()
-SSDspread.head()
-
-coefficients = pt.cointegration(priceAf, priceBf)
-coefficients
-
-CoSpreadF = pt.CointegrationSpread(priceA, priceB, formPeriod, formPeriod)
-CoSpreadF.head()
-
-CoSpreadTr = pt.CointegrationSpread(priceA, priceB, formPeriod, tradePeriod)
-CoSpreadTr.describe()
-
-bound = pt.calBound(priceA, priceB, 'Cointegration', formPeriod, width=1.2)
-bound
 
 # 配对交易实测
 # 提取形成期数据
@@ -254,8 +131,7 @@ plt.axhline(y=mu - 1.5 * sd, color='green', ls='--', lw=2.5)
 plt.axhline(y=mu + 2.5 * sd, color='red', ls='-.', lw=3)
 plt.axhline(y=mu - 2.5 * sd, color='red', ls='-.', lw=3)
 
-level = (
-float('-inf'), mu - 2.5 * sd, mu - 1.5 * sd, mu - 0.2 * sd, mu + 0.2 * sd, mu + 1.5 * sd, mu + 2.5 * sd, float('inf'))
+level = (float('-inf'), mu - 2.5 * sd, mu - 1.5 * sd, mu - 0.2 * sd, mu + 0.2 * sd, mu + 1.5 * sd, mu + 2.5 * sd, float('inf'))
 
 prcLevel = pd.cut(CoSpreadT, level, labels=False) - 3
 
