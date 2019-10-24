71722f5d58448e5ad302fcf9f22083757e3abd96
MuSaCN <435116098@qq.com>
2019-10-24 16:53:22 +0800

    update

--- My_Python_Items/AutoMql4Backups.py
+++ My_Python_Items/AutoMql4Backups.py
@@ -1,66 +1,35 @@
 # Author:Zhang Yuan
 import MyPackage
-import os
-__mypath__ = MyPackage.MyClass_Path.MyClass_Path()  #路径类
-myfile = MyPackage.MyClass_File.MyClass_File()      #文件操作类
 
-# ---客户端文件夹
-terminalPath="C:\\Users\\i2011\\AppData\\Roaming\\MetaQuotes\\Terminal\\F7DC4A11FD305E0AA6ED39F4697586F7"
-# ---定位MQL4文件夹
-MQL4Path = terminalPath+"\\MQL4"
-# ---需要操作的文件夹
-ExpertsPath = MQL4Path+"\\Experts"
-FilesPath = MQL4Path + "\\Files"
-IncludePath = MQL4Path + "\\Include"
-IndicatorsPath = MQL4Path + "\\Indicators"
-LogsPath = MQL4Path + "\\Logs"
-ScriptsPath = MQL4Path + "\\Scripts"
-print("------客户端目录：",terminalPath,
-      "------MQL4文件夹目录：",MQL4Path,
-      "------需要操作的文件夹：",
-      ExpertsPath,FilesPath,IncludePath,IndicatorsPath,LogsPath,ScriptsPath,sep="\n")
+Mql4Path = "C:\\Users\\i2011\\AppData\\Roaming\\MetaQuotes\\Terminal\\F7DC4A11FD305E0AA6ED39F4697586F7\\MQL4"
 
-# ---定义Mql4目录的复制函数
-def Mql4DirCopy(Path,subPath):
-    print("------{}文件夹开始备份------".format(subPath))
-    print("{0}文件夹进行备份：{1} - 副本".format(subPath,subPath))
-    Source = Path + "\\" + subPath
-    Destination = Path + "\\" + subPath + " - 副本"
-    myfile.copyDirOrFile(source=Source, destination=Destination,DirRemove=True)
-    print("{} - 副本 备份完成".format(subPath))
-# ---My_Experts, My_Include, My_Indicators, My_Scripts操作
-Mql4DirCopy(ExpertsPath,"MyExperts_MQL4")
-Mql4DirCopy(IncludePath,"MyClass_MQL4")
-Mql4DirCopy(IndicatorsPath,"MyIndicators_MQL4")
-Mql4DirCopy(ScriptsPath,"MyScripts_MQL4")
+myMql4 = MyPackage.MyClass_Mql.MyClass_MqlBackups(Mql4Path,isPrint=True)
 
-# ---定义Mql4目录的清理函数
-def Mql4DirRemove(Path, ignoreFolder):
-    name = os.path.split(Path)[1]
-    print("------开始清理{}文件夹------".format(name))
-    print("清空{0}文件夹：忽略里面 {1} 子文件夹".format(name,','.join(ignoreFolder)))
-    myfile.removeDirOrFile(Path, onlyContent=True, ignoreFolder=ignoreFolder, ProtectiveCheck=False)
-    print("{}文件夹清理完毕".format(name))
-# ---Files, Logs操作
-Mql4DirRemove(FilesPath,ignoreFolder=[])
-Mql4DirRemove(LogsPath,ignoreFolder=[])
+# ---MyExperts_MQL4, MyClass_MQL4, MyIndicators_MQL4, MyScripts_MQL4的复制操作
+myMql4.MqlDirCopy(myMql4.ExpertsPath,"MyExperts_MQL4")
+myMql4.MqlDirCopy(myMql4.IncludePath,"MyClass_MQL4")
+myMql4.MqlDirCopy(myMql4.IndicatorsPath,"MyIndicators_MQL4")
+myMql4.MqlDirCopy(myMql4.ScriptsPath,"MyScripts_MQL4")
+
+# ---Files, Logs的清理操作
+myMql4.MqlDirRemove(myMql4.FilesPath,ignoreFolder=[])
+myMql4.MqlDirRemove(myMql4.LogsPath,ignoreFolder=[])
 
 # ---MQL4文件夹备份
 print("------开始压缩MQL4文件夹------")
-needZip = MQL4Path # 需压缩的目录
+needZip = Mql4Path # 需压缩的目录
 # 备份到OneDrive的Work-Python备份文件夹
-OneDrive_Mql4 = myfile.ZipDir(needZip, zipPath=__mypath__.GetOneDrivePath() + "\\Work-Mql备份" , zipName=None, autoName=True)
+OneDrive_Mql4 = myMql4.myfile.ZipDir(needZip, zipPath=myMql4.mypath.GetOneDrivePath() + "\\Work-Mql备份" , zipName=None, autoName=True)
 print("MQL4压缩文件保存完成，{}".format(OneDrive_Mql4))
 
 # ---上传到Baidu云
 print("------开始上传压缩文件到Baidu云盘------")
-locallist = []
-locallist.append(OneDrive_Mql4)
 myBaidu= MyPackage.MyClass_WebCrawler.MyClass_BaiduPan()    #百度网盘交互类
+needUpload = OneDrive_Mql4
 remotePath = "\\MyMql4Backups\\"
 # 开始批量上传
-for i in range(len(locallist)):
-    print("{} 开始上传.".format(locallist[i]))
-    myBaidu.upload(localpath=locallist[i], remotepath=remotePath, ondup="overwrite")
-    print("{} 上传完成.".format(locallist[i]))
+print("{} 开始上传.".format(needUpload))
+myBaidu.upload(localpath=needUpload, remotepath=remotePath, ondup="overwrite")
+print("{} 上传完成.".format(needUpload))
+
 print("全部完成！")
--- My_Python_Items/AutoMql5Backups.py
+++ My_Python_Items/AutoMql5Backups.py
@@ -1,67 +1,35 @@
 # Author:Zhang Yuan
 import MyPackage
-import os
-__mypath__ = MyPackage.MyClass_Path.MyClass_Path()  #路径类
-myfile = MyPackage.MyClass_File.MyClass_File()      #文件操作类
 
-# ---客户端文件夹
-terminalPath="C:\\Users\\i2011\\AppData\\Roaming\\MetaQuotes\\Terminal\\6E8A5B613BD795EE57C550F7EF90598D"
-# ---定位MQL5文件夹
-MQL5Path = terminalPath+"\\MQL5"
-# ---需要操作的文件夹
-ExpertsPath = MQL5Path+"\\Experts"
-FilesPath = MQL5Path + "\\Files"
-IncludePath = MQL5Path + "\\Include"
-IndicatorsPath = MQL5Path + "\\Indicators"
-LogsPath = MQL5Path + "\\Logs"
-ScriptsPath = MQL5Path + "\\Scripts"
-print("------客户端目录：",terminalPath,
-      "------MQL5文件夹目录：",MQL5Path,
-      "------需要操作的文件夹：",
-      ExpertsPath,FilesPath,IncludePath,IndicatorsPath,LogsPath,ScriptsPath,sep="\n")
+Mql5Path = "C:\\Users\\i2011\\AppData\\Roaming\\MetaQuotes\\Terminal\\6E8A5B613BD795EE57C550F7EF90598D\\MQL5"
 
-# ---定义Mql5目录的复制函数
-def Mql5DirCopy(Path,subPath):
-    print("------{}文件夹开始备份------".format(subPath))
-    print("{0}文件夹进行备份：{1} - 副本".format(subPath,subPath))
-    Source = Path + "\\" + subPath
-    Destination = Path + "\\" + subPath + " - 副本"
-    myfile.copyDirOrFile(source=Source, destination=Destination,DirRemove=True)
-    print("{} - 副本 备份完成".format(subPath))
-# ---My_Experts, My_Include, My_Indicators, My_Scripts操作
-Mql5DirCopy(ExpertsPath,"My_Experts")
-Mql5DirCopy(IncludePath,"My_Include")
-Mql5DirCopy(IndicatorsPath,"My_Indicators")
-Mql5DirCopy(ScriptsPath,"My_Scripts")
+myMql5 = MyPackage.MyClass_Mql.MyClass_MqlBackups(Mql5Path,isPrint=True)
 
-# ---定义Mql5目录的清理函数
-def Mql5DirRemove(Path, ignoreFolder):
-    name = os.path.split(Path)[1]
-    print("------开始清理{}文件夹------".format(name))
-    print("清空{0}文件夹：忽略里面 {1} 子文件夹".format(name,','.join(ignoreFolder)))
-    myfile.removeDirOrFile(Path, onlyContent=True, ignoreFolder=ignoreFolder, ProtectiveCheck=False)
-    print("{}文件夹清理完毕".format(name))
-# ---Files, Logs操作
-Mql5DirRemove(FilesPath,ignoreFolder=["SPSS"])
-Mql5DirRemove(LogsPath,ignoreFolder=[])
+# ---My_Experts, My_Include, My_Indicators, My_Scripts复制备份操作
+myMql5.MqlDirCopy(myMql5.ExpertsPath,"My_Experts")
+myMql5.MqlDirCopy(myMql5.IncludePath,"My_Include")
+myMql5.MqlDirCopy(myMql5.IndicatorsPath,"My_Indicators")
+myMql5.MqlDirCopy(myMql5.ScriptsPath,"My_Scripts")
 
-# ---MQL5文件夹备份
+# ---Files, Logs清理操作
+myMql5.MqlDirRemove(myMql5.FilesPath,ignoreFolder=["SPSS"])
+myMql5.MqlDirRemove(myMql5.LogsPath,ignoreFolder=[])
+
+# ---MQL5文件夹备份到OneDrive的Work-Python备份文件夹
 print("------开始压缩MQL5文件夹------")
-needZip = MQL5Path # 需压缩的目录
-# 备份到OneDrive的Work-Python备份文件夹
-OneDrive_Mql5 = myfile.ZipDir(needZip, zipPath=__mypath__.GetOneDrivePath() + "\\Work-Mql备份" , zipName=None, autoName=True)
+needZip = Mql5Path # 需压缩的目录
+OneDrive_Mql5 = myMql5.myfile.ZipDir(needZip, zipPath=myMql5.mypath.GetOneDrivePath() + "\\Work-Mql备份" , zipName=None, autoName=True)
 print("MQL5压缩文件保存完成，{}".format(OneDrive_Mql5))
 
 # ---上传到Baidu云
 print("------开始上传压缩文件到Baidu云盘------")
-locallist = []
-locallist.append(OneDrive_Mql5)
-myBaidu= MyPackage.MyClass_WebCrawler.MyClass_BaiduPan()    #百度网盘交互类
+myBaidu= MyPackage.MyClass_WebCrawler.MyClass_BaiduPan()    # 百度网盘交互类
+needUpload = OneDrive_Mql5
 remotePath = "\\MyMql5Backups\\"
 # 开始批量上传
-for i in range(len(locallist)):
-    print("{} 开始上传.".format(locallist[i]))
-    myBaidu.upload(localpath=locallist[i], remotepath=remotePath, ondup="overwrite")
-    print("{} 上传完成.".format(locallist[i]))
+print("{} 开始上传.".format(needUpload))
+myBaidu.upload(localpath=needUpload, remotepath=remotePath, ondup="overwrite")
+print("{} 上传完成.".format(needUpload))
+
 print("全部完成！")
 
