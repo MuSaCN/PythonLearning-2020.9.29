6849a7224944e4df32e07dae5afb74690fc8c5cf
MuSaCN <435116098@qq.com>
2019-10-26 10:36:59 +0800

    update

---
+++ test/backtrader_debug2.py
@@ -0,0 +1,128 @@
+# Author:Zhang Yuan
+import MyPackage
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+#------------------------------------------------------------
+__mypath__ = MyPackage.MyClass_Path.MyClass_Path("\\test")  #路径类
+myfile = MyPackage.MyClass_File.MyClass_File()  #文件操作类
+myplt = MyPackage.MyClass_Plot.MyClass_Plot()  #直接绘图类(单个图窗)
+myfig = MyPackage.MyClass_Plot.MyClass_Figure(AddFigure=False)  #对象式绘图类(可多个图窗)
+mypltpro = MyPackage.MyClass_PlotPro.MyClass_PlotPro()  #Plot高级图系列
+myfigpro = MyPackage.MyClass_PlotPro.MyClass_FigurePro(AddFigure=False)  #Figure高级图系列
+mynp = MyPackage.MyClass_Array.MyClass_NumPy()  #多维数组类(整合Numpy)
+mypd = MyPackage.MyClass_Array.MyClass_Pandas()  #矩阵数组类(整合Pandas)
+mypdpro = MyPackage.MyClass_ArrayPro.MyClass_PandasPro()  #高级矩阵数组类
+mytime = MyPackage.MyClass_Time.MyClass_Time()  #时间类
+myDA = MyPackage.MyClass_DataAnalysis.MyClass_DataAnalysis()  #数据分析类
+myBT = MyPackage.MyClass_BackTest.MyClass_BackTest()  #回测类
+myWebQD = MyPackage.MyClass_WebCrawler.MyClass_WebQuotesDownload()  #金融行情下载类
+#------------------------------------------------------------
+
+# ---获得数据
+Path = "C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\量化投资以python为工具\\数据及源代码\\033"
+CJSecurities = pd.read_csv(Path + '\\CJSecurities.csv', index_col=1, parse_dates=True)
+CJSecurities = CJSecurities.iloc[:, 1:]
+data = CJSecurities["2015"]
+
+
+# ---基础设置
+myBT = MyPackage.MyClass_BackTest.MyClass_BackTest()  #回测类
+myBT.Cash(9999)
+myBT.AddBarsData(data,fromdate=None,todate=None)
+
+# ---策略函数
+@myBT.OnInit
+def __init__():
+    print(myBT.close(0), "init")  # 此时没有递归，表示数据的最后一个
+
+# ---增加策略1
+@myBT.OnNext
+def next():
+    # Simply log the closing price of the series from the reference
+    if myBT.close(0) < myBT.close(1):
+        # current close less than previous close
+        if myBT.close(1) < myBT.close(2):
+            myBT.bt.Strategy.buy()
+myBT.addstrategy()
+# ---运行
+myBT.run(plot = True)
+
+
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+
+# Import the backtrader platform
+import backtrader as bt
+
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.dataclose[0] < self.dataclose[-1]:
+            # current close less than previous close
+
+            if self.dataclose[-1] < self.dataclose[-2]:
+                # previous close less than the previous close
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.buy()
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, '../../datas/orcl-1995-2014.txt')
+
+    # Create a Data Feed
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2000, 12, 31),
+        # Do not pass values after this date
+        reverse=False)
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
