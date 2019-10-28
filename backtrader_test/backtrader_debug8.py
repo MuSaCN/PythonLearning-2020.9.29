ce0d5e2a6f987ccc4b778db7c93db12b812749a8
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-10-28 23:29:45 +0800

    update

---
+++ backtrader_test/backtrader_debug8.py
@@ -0,0 +1,71 @@
+# Author:Zhang Yuan
+import MyPackage
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+#------------------------------------------------------------
+__mypath__ = MyPackage.MyClass_Path.MyClass_Path("\\backtrader_test")  #路径类
+myfile = MyPackage.MyClass_File.MyClass_File()  #文件操作类
+myplt = MyPackage.MyClass_Plot.MyClass_Plot()  #直接绘图类(单个图窗)
+myfig = MyPackage.MyClass_Plot.MyClass_Figure(AddFigure=False)  #对象式绘图类(可多个图窗)
+mypltpro = MyPackage.MyClass_PlotPro.MyClass_PlotPro()  #Plot高级图系列
+myfigpro = MyPackage.MyClass_PlotPro.MyClass_FigurePro(AddFigure=False)  #Figure高级图系列
+mynp = MyPackage.MyClass_Array.MyClass_NumPy()  #多维数组类(整合Numpy)
+mypd = MyPackage.MyClass_Array.MyClass_Pandas()  #矩阵数组类(整合Pandas)
+mypdpro = MyPackage.MyClass_ArrayPro.MyClass_PandasPro()  #高级矩阵数组类
+mytime = MyPackage.MyClass_Time.MyClass_Time()  #时间类
+myDA = MyPackage.MyClass_DataAnalysis.MyClass_DataAnalysis()  #数据分析类
+myBT = MyPackage.MyClass_BackTest.MyClass_BackTest()  #回测类
+myWebQD = MyPackage.MyClass_WebCrawler.MyClass_WebQuotesDownload()  #金融行情下载类
+#------------------------------------------------------------
+
+
+import backtrader as bt
+cerebro = bt.Cerebro()
+# ---read Data
+Path = "C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\量化投资以python为工具\\数据及源代码\\033"
+CJSecurities = pd.read_csv(Path + '\\CJSecurities.csv', index_col=1, parse_dates=True)
+CJSecurities = CJSecurities.iloc[:, 1:]
+data0 = CJSecurities
+if "openinterest" not in data0.columns:
+    data0['openinterest'] = 0
+data = bt.feeds.PandasData(dataname=data0)
+
+cerebro.adddata(data)
+cerebro.broker.setcash(1000.0)
+cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+cerebro.broker.setcommission(commission=0.0)
+
+
+class TestStrategy(bt.Strategy):
+    params = ( ('maperiod', 15), )
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.maperiod)
+        self.bars_executed = 0
+    def next(self):
+        if not self.position:
+            if self.dataclose[0] > self.sma[0]:
+                self.buy()
+        else:
+            if len(self) >= self.bars_executed + 5:
+                self.sell()
+    def notify_order(self, order):
+        self.bars_executed = len(self)
+    def stop(self):
+        print(self.params.maperiod, self.broker.getvalue())
+
+# cerebro.addstrategy(TestStrategy)
+cerebro.optstrategy(TestStrategy,maperiod=range(5, 10)) # *************
+
+cerebro.run()
+
+cerebro.plot(iplot=False)
+
+
+
