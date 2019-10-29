d0a69b389e7357265e4c31cce11f73f49cb570a5
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-10-29 16:30:59 +0800

    update

--- backtrader_test/backtrader_debug8.py
+++ backtrader_test/backtrader_debug8.py
@@ -20,47 +20,66 @@ mypd = MyPackage.MyClass_Array.MyClass_Pandas()  #矩阵数组类(整合Pandas)
 mypdpro = MyPackage.MyClass_ArrayPro.MyClass_PandasPro()  #高级矩阵数组类
 mytime = MyPackage.MyClass_Time.MyClass_Time()  #时间类
 myDA = MyPackage.MyClass_DataAnalysis.MyClass_DataAnalysis()  #数据分析类
-myBT = MyPackage.MyClass_BackTest.MyClass_BackTest()  #回测类
+myBTE = MyPackage.MyClass_BackTestEvent.MyClass_BackTestEvent()  # 事件驱动型回测类
+myBTV = MyPackage.MyClass_BackTestVector.MyClass_BackTestVector() # 向量型回测类
 myWebQD = MyPackage.MyClass_WebCrawler.MyClass_WebQuotesDownload()  #金融行情下载类
 #------------------------------------------------------------
 
-
-import backtrader as bt
-cerebro = bt.Cerebro()
-# ---read Data
+# ---获得数据
 Path = "C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\量化投资以python为工具\\数据及源代码\\033"
 CJSecurities = pd.read_csv(Path + '\\CJSecurities.csv', index_col=1, parse_dates=True)
 CJSecurities = CJSecurities.iloc[:, 1:]
 data0 = CJSecurities
-if "openinterest" not in data0.columns:
-    data0['openinterest'] = 0
-data = bt.feeds.PandasData(dataname=data0)
 
-cerebro.adddata(data)
-cerebro.broker.setcash(100000.0)
+# ---基础设置
+myBTE = MyPackage.MyClass_BackTestEvent.MyClass_BackTestEvent()  #回测类
+myBTE.ValueCash(100000)
+myBTE.setcommission(0.001)
+myBTE.AddBarsData(data0,fromdate=None,todate=None)
 
-class TestStrategy(bt.Strategy):
-    params = ( ('maperiod', 15), )
+
+class GeneralStrategy(myBTE.bt.Strategy):
+    # ---设定参数，必须写params，以self.params.Para0索引，可用于优化，内部必须要有逗号
+    params = ( ('Para0', 15), )
+
+    # ---只开头执行一次
     def __init__(self):
-        self.dataclose = self.datas[0].close
-        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0], period=self.params.maperiod)
-        self.bars_executed = 0
+        print("init", len(self))
+        self.smahandle = myBTE.addIndi_SMA(0, period=self.params.Para0)
+        self.sma = lambda x: self.smahandle[-x]
+        self.barscount = 0
+
+    # ---每一个Bar迭代执行一次。next()执行完就进入下一个bar
     def next(self):
+        print("next sma:", self.sma(0), self.sma(1), self.sma(2))
         if not self.position:
-            if self.dataclose[0] > self.sma[0]:
+            if myBTE.close(0) > self.sma(0):
                 self.buy()
         else:
-            if len(self) >= self.bars_executed + 5:
+            if len(self) >= self.barscount + 5:
                 self.sell()
+
+    # ---策略每笔订单通知函数。已经进入下一个bar，且在next()之前执行
     def notify_order(self, order):
-        self.bars_executed = len(self)
+        if myBTE.orderStatusCheck(order,True) == True:
+            self.barscount = len(self)
+
+    # ---策略每笔交易通知函数。已经进入下一个bar，且在notify_order()之后，next()之前执行。
+    def notify_trade(self, trade):
+        myBTE.tradeStatus(trade,isclosed=False)
+
+    # ---策略加载完会触发此语句
     def stop(self):
-        print(self.params.maperiod, self.broker.getvalue())
+        print("stop(): " , myBTE.ValueCash(), self.sma(0), self.sma(1), self.sma(2))
 
-cerebro.optstrategy(TestStrategy,maperiod=range(5, 10)) # *************
-cerebro.run(maxcpus=1)
+# ---run
+myBTE.addstrategy(GeneralStrategy)
+myBTE.run(plot = True)
 
-cerebro.plot(iplot=False)
+# ---opt
+myBTE.optstrategy(GeneralStrategy,Para0=range(5,10))
+myBTE.run(plot = False)
+
 
 
 
