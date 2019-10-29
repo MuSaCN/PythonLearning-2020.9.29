ec1e5f39919e4ed07f455c3d45eb31be93090e06
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-10-30 04:15:07 +0800

    update

--- backtrader_test/backtrader模板.py
+++ backtrader_test/backtrader模板.py
@@ -31,9 +31,9 @@ CJSecurities = pd.read_csv(Path + '\\CJSecurities.csv', index_col=1, parse_dates
 CJSecurities = CJSecurities.iloc[:, 1:]
 data0 = CJSecurities
 
-class GeneralStrategy(myBTE.bt.Strategy):
+class ABCStrategy(myBTE.bt.Strategy):
     # ---设定参数，必须写params，以self.params.Para0索引，可用于优化，内部必须要有逗号
-    params = ( ('Para0', 15), )
+    params = (('Para0', 15),)
 
     # ---只开头执行一次
     def __init__(self):
@@ -41,12 +41,13 @@ class GeneralStrategy(myBTE.bt.Strategy):
         self.smahandle = myBTE.addIndi_SMA(0, period=self.params.Para0)
         self.sma = lambda x: self.smahandle[-x]
         self.barscount = 0
+        self.close = self.datas[0].close
 
     # ---每一个Bar迭代执行一次。next()执行完就进入下一个bar
     def next(self):
         print("next sma:", self.sma(0), self.sma(1), self.sma(2))
         if not self.position:
-            if myBTE.close(0) > self.sma(0):
+            if self.close[0] > self.sma(0):
                 self.buy()
         else:
             if len(self) >= self.barscount + 5:
@@ -54,32 +55,35 @@ class GeneralStrategy(myBTE.bt.Strategy):
 
     # ---策略每笔订单通知函数。已经进入下一个bar，且在next()之前执行
     def notify_order(self, order):
-        if myBTE.orderStatusCheck(order,True) == True:
+        if myBTE.orderStatusCheck(order, True) == True:
             self.barscount = len(self)
 
     # ---策略每笔交易通知函数。已经进入下一个bar，且在notify_order()之后，next()之前执行。
     def notify_trade(self, trade):
-        myBTE.tradeStatus(trade,isclosed=False)
+        myBTE.tradeStatus(trade, isclosed=False)
 
     # ---策略加载完会触发此语句
     def stop(self):
-        print("stop(): " , myBTE.ValueCash(), self.sma(0), self.sma(1), self.sma(2))
+        print("stop(): ", myBTE.ValueCash(), self.sma(0), self.sma(1), self.sma(2))
 
 # ---run
-# myBTE.addstrategy(GeneralStrategy)
-# myBTE.run(plot = True)
 from multiprocessing import freeze_support
 # ---opt
 if __name__ == '__main__':
     freeze_support()
+
     # ---基础设置
     myBTE = MyPackage.MyClass_BackTestEvent.MyClass_BackTestEvent()  # 回测类
     myBTE.ValueCash(100000)
     myBTE.setcommission(0.001)
     myBTE.AddBarsData(data0, fromdate=None, todate=None)
 
-    myBTE.optstrategy(GeneralStrategy,Para0=range(5,100))
-    myBTE.run(maxcpus=8,plot = False)
+    # myBTE.addstrategy(GeneralStrategy)
+    # myBTE.run(plot=True)
+
+    myBTE.optstrategy(ABCStrategy,Para0=range(5,100))
+    myBTE.cerebro.run(maxcpus=None)
+    # myBTE.run(maxcpus=None,plot = False)
 
 
 
