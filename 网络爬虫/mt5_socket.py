99583a467ba49d9cff022a7fb2b2f405427c5470
MuSaCN <435116098@qq.com>
2019-11-04 17:53:36 +0800

    update

--- 网络爬虫/mt5_socket.py
+++ 网络爬虫/mt5_socket.py
@@ -22,6 +22,7 @@ mypdpro = MyArray.MyClass_PandasPro()  # 高级矩阵数组类
 myDA = MyDataAnalysis.MyClass_DataAnalysis()  # 数据分析类
 # myMql = MyMql.MyClass_MqlBackups() # Mql备份类
 # myMT5 = MyMql.MyClass_ConnectMT5(connect=False) # Python链接MetaTrader5客户端类
+# mySMT5 = MyMql.MyClass_SocketMT5() # 以Socket方式建立python与MT5通信类
 # myDefault = MyDefault.MyClass_Default_Matplotlib() # matplotlib默认设置
 # myBaidu = MyWebCrawler.MyClass_BaiduPan() # Baidu网盘交互类
 myWebQD = MyWebCrawler.MyClass_WebQuotesDownload()  # 金融行情下载类
@@ -29,20 +30,11 @@ myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
 myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
 #------------------------------------------------------------
 
-myMT5 = MyMql.MyClass_ConnectMT5(connect=True)
-rates = myMT5.MT5CopyRatesFrom("EURUSD",myMT5.mt5.MT5_TIMEFRAME_H1,[2019,11,1],10)
-rates = myMT5.MT5CopyRatesFromPos("GBPUSD",myMT5.mt5.MT5_TIMEFRAME_D1,1,10)
-rates = myMT5.MT5CopyRatesRange("EURUSD",myMT5.mt5.MT5_TIMEFRAME_H1,[2019,10,1],[2019,10,3])
-rates1 = myMT5.RatesToDataFrame(rates,False)
-myMT5.DataTimeZoneOffset(rates1)
+mySMT5 = MyMql.MyClass_SocketMT5()
 
-ticks = myMT5.MT5CopyTicksFrom("EURUSD",myMT5.mt5.MT5_COPY_TICKS_ALL,[2019,10,20],1000)
-ticks = myMT5.MT5CopyTicksRange("EURUSD",myMT5.mt5.MT5_COPY_TICKS_ALL,[2019,10,10],[2019,10,11])
-ticks1 = myMT5.TicksToDataFrame(ticks,False)
-myMT5.DataTimeZoneOffset(ticks1)
-
-
-
-
+while True:
+    msg = mySMT5.ReceiveMessage()
+    print("接收到的信息为：",msg)
+    mySMT5.Send()
 
 
