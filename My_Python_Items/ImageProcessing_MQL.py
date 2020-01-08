19ac6fd97253f284fc3bb922d6985e0975a2b13a
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-08 17:00:02 +0800

    update

--- My_Python_Items/ImageProcessing_MQL.py
+++ My_Python_Items/ImageProcessing_MQL.py
@@ -39,14 +39,14 @@ for image in imagelist:
             # 生成logo_DEMO
             myImage.__init__(file_in = file_out, draw = True)
             file_demo = filepath + "\\logo\\" + name[0] + "_" +i + "_DEMO." + name[1]
-            myImage.textOnImage(i, -1, -1, 30, file_demo, save=False)
-            myImage.textOnImage("MoreTimeFrame", -1, 120, 25, file_demo, save=False) # ***每次需修改***
-            myImage.textOnImage("DEMO", -1, 150, 30, file_demo, save=True)
+            myImage.textOnImage(i, -1, -1, 35, file_demo, save=False)
+            #myImage.textOnImage("MoreTimeFrame", -1, 120, 25, file_demo, save=False) # ***每次需修改***
+            myImage.textOnImage("DEMO", -1, 155, 30, file_demo, save=True)
             # 生成logo_Paid
             myImage.__init__(file_in=file_out, draw=True)
             file_paid = filepath + "\\logo\\" + name[0] + "_" +i + "_Paid." + name[1]
-            myImage.textOnImage(i, -1, -1, 30, file_paid, save=True)
-            myImage.textOnImage("MoreTimeFrame", -1, 120, 25, file_paid, save=True)  # ***每次需修改***
+            myImage.textOnImage(i, -1, -1, 35, file_paid, save=True)
+            #myImage.textOnImage("MoreTimeFrame", -1, 120, 25, file_paid, save=True)  # ***每次需修改***
     else:
         # 不是logo文件，则转成640*480
         width = 640; height = 480
