b0be5a0d0fbda92457939012dcc57f3bd7242174
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-14 12:26:49 +0800

    update

--- Python大战机器学习/chapter1.py
+++ Python大战机器学习/chapter1.py
@@ -30,9 +30,7 @@ myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
 myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
 #------------------------------------------------------------
 
-from sklearn import datasets, linear_model, discriminant_analysis, model_selection
 
-# -*- coding: utf-8 -*-
 """
     广义线性模型
     ~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,8 +40,7 @@ from sklearn import datasets, linear_model, discriminant_analysis, model_selecti
     :copyright: (c) 2016 by the huaxz1986.
     :license: lgpl-3.0, see LICENSE for more details.
 """
-import matplotlib.pyplot as plt
-import numpy as np
+
 from sklearn import datasets, linear_model,model_selection
 
 def load_data():
@@ -53,8 +50,7 @@ def load_data():
     :return: 一个元组，用于回归问题。元组元素依次为：训练样本集、测试样本集、训练样本集对应的值、测试样本集对应的值
     '''
     diabetes = datasets.load_diabetes()#使用 scikit-learn 自带的一个糖尿病病人的数据集
-    return model_selection.train_test_split(diabetes.data,diabetes.target,
-		test_size=0.25,random_state=0) # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
+    return model_selection.train_test_split(diabetes.data,diabetes.target,test_size=0.25,random_state=0) # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
 def test_LinearRegression(*data):
     '''
     测试 LinearRegression 的用法
@@ -68,6 +64,7 @@ def test_LinearRegression(*data):
     print('Coefficients:%s, intercept %.2f'%(regr.coef_,regr.intercept_))
     print("Residual sum of squares: %.2f"% np.mean((regr.predict(X_test) - y_test) ** 2))
     print('Score: %.2f' % regr.score(X_test, y_test))
+
 if __name__=='__main__':
     X_train,X_test,y_train,y_test=load_data() # 产生用于回归问题的数据集
     test_LinearRegression(X_train,X_test,y_train,y_test) # 调用 test_LinearRegression
