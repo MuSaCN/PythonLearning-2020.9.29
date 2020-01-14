8ade2a6ebe6b7f20bdf508fe9b5e7aa7da90ac73
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-14 12:20:01 +0800

    update

---
+++ Python大战机器学习/chapter1.py
@@ -0,0 +1,73 @@
+# Author:Zhang Yuan
+from MyPackage import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+#------------------------------------------------------------
+__mypath__ = MyPath.MyClass_Path("\\Python大战机器学习")  # 路径类
+myfile = MyFile.MyClass_File()  # 文件操作类
+mytime = MyTime.MyClass_Time()  # 时间类
+myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
+mypltpro = MyPlot.MyClass_PlotPro()  # Plot高级图系列
+myfig = MyPlot.MyClass_Figure(AddFigure=False)  # 对象式绘图类(可多个图窗)
+myfigpro = MyPlot.MyClass_FigurePro(AddFigure=False)  # Figure高级图系列
+mynp = MyArray.MyClass_NumPy()  # 多维数组类(整合Numpy)
+mypd = MyArray.MyClass_Pandas()  # 矩阵数组类(整合Pandas)
+mypdpro = MyArray.MyClass_PandasPro()  # 高级矩阵数组类
+myDA = MyDataAnalysis.MyClass_DataAnalysis()  # 数据分析类
+# myMql = MyMql.MyClass_MqlBackups() # Mql备份类
+# myMT5 = MyMql.MyClass_ConnectMT5(connect=False) # Python链接MetaTrader5客户端类
+# myDefault = MyDefault.MyClass_Default_Matplotlib() # matplotlib默认设置
+# myBaidu = MyWebCrawler.MyClass_BaiduPan() # Baidu网盘交互类
+# myImage = MyImage.MyClass_ImageProcess()  # 图片处理类
+myWebQD = MyWebCrawler.MyClass_WebQuotesDownload()  # 金融行情下载类
+myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
+myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
+#------------------------------------------------------------
+
+from sklearn import datasets, linear_model, discriminant_analysis, model_selection
+
+# -*- coding: utf-8 -*-
+"""
+    广义线性模型
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    LinearRegression
+
+    :copyright: (c) 2016 by the huaxz1986.
+    :license: lgpl-3.0, see LICENSE for more details.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model,model_selection
+
+def load_data():
+    '''
+    加载用于回归问题的数据集
+
+    :return: 一个元组，用于回归问题。元组元素依次为：训练样本集、测试样本集、训练样本集对应的值、测试样本集对应的值
+    '''
+    diabetes = datasets.load_diabetes()#使用 scikit-learn 自带的一个糖尿病病人的数据集
+    return model_selection.train_test_split(diabetes.data,diabetes.target,
+		test_size=0.25,random_state=0) # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
+def test_LinearRegression(*data):
+    '''
+    测试 LinearRegression 的用法
+
+    :param data: 可变参数。它是一个元组，这里要求其元素依次为：训练样本集、测试样本集、训练样本的值、测试样本的值
+    :return: None
+    '''
+    X_train,X_test,y_train,y_test=data
+    regr = linear_model.LinearRegression()
+    regr.fit(X_train, y_train)
+    print('Coefficients:%s, intercept %.2f'%(regr.coef_,regr.intercept_))
+    print("Residual sum of squares: %.2f"% np.mean((regr.predict(X_test) - y_test) ** 2))
+    print('Score: %.2f' % regr.score(X_test, y_test))
+if __name__=='__main__':
+    X_train,X_test,y_train,y_test=load_data() # 产生用于回归问题的数据集
+    test_LinearRegression(X_train,X_test,y_train,y_test) # 调用 test_LinearRegression
