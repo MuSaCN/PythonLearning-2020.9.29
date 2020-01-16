ce074b11b432fb91783e667b1a478ab6cc50ae93
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-16 19:14:31 +0800

    update

--- Python大战机器学习/chapter2.py
+++ Python大战机器学习/chapter2.py
@@ -134,7 +134,7 @@ ax.set_title("Decision Tree Classification")
 ax.legend(framealpha=0.5,loc='best')
 plt.show()
 
-
+# ---
 from sklearn.datasets import load_iris
 from sklearn import tree
 X, y = load_iris(return_X_y=True)
@@ -143,16 +143,6 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, y)
 tree.plot_tree(clf.fit(X, y))
 
-import graphviz
-dot_data = tree.export_graphviz(clf, out_file=None)
-graph = graphviz.Source(dot_data)
-graph.render("iris")
 
-dot_data = tree.export_graphviz(clf, out_file=None,
-                     feature_names=iris.feature_names,
-                     class_names=iris.target_names,
-                     filled=True, rounded=True,
-                     special_characters=True)
-graph = graphviz.Source(dot_data)
-graph
+
 
