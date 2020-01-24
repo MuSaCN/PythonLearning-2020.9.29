ddf769fcf9eb2359418702a1c5edd9243581f55f
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-24 14:27:43 +0800

    update

--- Python大战机器学习/chapter5.py
+++ Python大战机器学习/chapter5.py
@@ -63,7 +63,7 @@ pca.fit(X)
 print('explained variance ratio : %s'% str(pca.explained_variance_ratio_))
 
 
-# ---核化线性降维
+# ---核化线性降维 KernelPCA
 from sklearn import decomposition
 iris = myML.DataPre.load_datasets("iris") # 使用 scikit-learn 自带的 iris 数据集
 X,y = iris.data,iris.target
@@ -147,10 +147,10 @@ plt.show()
 
 fig=plt.figure()
 Params=[(0.01,0.1),(0.01,0.2),(0.1,0.1),(0.1,0.2),(0.2,0.1),(0.2,0.2)]# sigmoid 核的参数组成的列表。
-    # 每个元素就是一种参数组合（依次为 gamma,coef0）
-    # gamma 取值为： 0.01，0.1，0.2
-    # coef0 取值为： 0.1,0.2
-    # 排列组合一共有 6 种组合
+# 每个元素就是一种参数组合（依次为 gamma,coef0）
+# gamma 取值为： 0.01，0.1，0.2
+# coef0 取值为： 0.1,0.2
+# 排列组合一共有 6 种组合
 for i,(gamma,r) in enumerate(Params):
     kpca=decomposition.KernelPCA(n_components=2,kernel='sigmoid',gamma=gamma,coef0=r)
     kpca.fit(X)
@@ -169,5 +169,133 @@ plt.suptitle("KPCA-sigmoid")
 plt.show()
 
 
+# ---MDS
+from sklearn import  manifold
+iris = myML.DataPre.load_datasets("iris") # 使用 scikit-learn 自带的 iris 数据集
+X,y = iris.data,iris.target
 
+# 测试 MDS 的用法
+for n in [4,3,2,1]: # 依次考察降维目标为 4维、3维、2维、1维
+    mds=manifold.MDS(n_components=n)
+    mds.fit(X)
+    print('stress(n_components=%d) : %s'% (n, str(mds.stress_)))
+
+# 绘制经过 使用 MDS 降维到二维之后的样本点
+mds=manifold.MDS(n_components=2)
+X_r=mds.fit_transform(X) #原始数据集转换到二维
+# 绘制二维图形
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+for label in np.unique(y):
+    position=y==label
+    ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
+ax.set_xlabel("X[0]")
+ax.set_ylabel("X[1]")
+ax.legend(loc="best")
+ax.set_title("MDS")
+plt.show()
+
+
+# ---Isomap
+from sklearn import  manifold
+iris = myML.DataPre.load_datasets("iris") # 使用 scikit-learn 自带的 iris 数据集
+X,y = iris.data,iris.target
+
+# 测试 Isomap 的用法
+for n in [4,3,2,1]: # 依次考察降维目标为 4维、3维、2维、1维
+    isomap=manifold.Isomap(n_components=n)
+    isomap.fit(X)
+    print('reconstruction_error(n_components=%d) : %s'% (n, isomap.reconstruction_error()) )
+
+# 测试 Isomap 中 n_neighbors 参数的影响，其中降维至 2维
+Ks=[1,5,25,y.size-1] # n_neighbors参数的候选值的集合
+fig=plt.figure()
+for i, k in enumerate(Ks):
+    isomap=manifold.Isomap(n_components=2,n_neighbors=k)
+    X_r=isomap.fit_transform(X)#原始数据集转换到二维
+    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 Isomap 的效果图
+    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
+        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
+    for label ,color in zip( np.unique(y),colors):
+        position=y==label
+        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"
+        %label,color=color)
+    ax.set_xlabel("X[0]")
+    ax.set_ylabel("X[1]")
+    ax.legend(loc="best")
+    ax.set_title("k=%d"%k)
+plt.suptitle("Isomap")
+plt.show()
+
+# 测试 Isomap 中 n_neighbors 参数的影响，其中降维至 1维
+Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
+fig=plt.figure()
+for i, k in enumerate(Ks):
+    isomap=manifold.Isomap(n_components=1,n_neighbors=k)
+    X_r=isomap.fit_transform(X)#原始数据集转换到 1 维
+    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 Isomap 的效果图
+    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
+        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
+    for label ,color in zip( np.unique(y),colors):
+        position=y==label
+        ax.scatter(X_r[position],np.zeros_like(X_r[position]),
+        label="target= %d"%label,color=color)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.legend(loc="best")
+    ax.set_title("k=%d"%k)
+plt.suptitle("Isomap")
+plt.show()
+
+
+# ---LLE
+from sklearn import  manifold
+iris = myML.DataPre.load_datasets("iris") # 使用 scikit-learn 自带的 iris 数据集
+X,y = iris.data,iris.target
+
+# 测试 LocallyLinearEmbedding 的用法
+for n in [4,3,2,1]:# 依次考察降维目标为 4维、3维、2维、1维
+    lle=manifold.LocallyLinearEmbedding(n_components=n)
+    lle.fit(X)
+    print('reconstruction_error(n_components=%d) : %s'% (n, lle.reconstruction_error_) )
+
+# 测试 LocallyLinearEmbedding 中 n_neighbors 参数的影响，其中降维至 2维
+Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
+fig=plt.figure()
+for i, k in enumerate(Ks):
+    lle=manifold.LocallyLinearEmbedding(n_components=2,n_neighbors=k)
+    X_r=lle.fit_transform(X)#原始数据集转换到二维
+    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 LocallyLinearEmbedding 的效果图
+    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
+        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
+    for label ,color in zip( np.unique(y),colors):
+        position=y==label
+        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"
+        %label,color=color)
+    ax.set_xlabel("X[0]")
+    ax.set_ylabel("X[1]")
+    ax.legend(loc="best")
+    ax.set_title("k=%d"%k)
+plt.suptitle("LocallyLinearEmbedding")
+plt.show()
+
+# 测试 LocallyLinearEmbedding 中 n_neighbors 参数的影响，其中降维至 1维
+Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
+fig=plt.figure()
+for i, k in enumerate(Ks):
+    lle=manifold.LocallyLinearEmbedding(n_components=1,n_neighbors=k)
+    X_r=lle.fit_transform(X)#原始数据集转换到 1 维
+    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 LocallyLinearEmbedding 的效果图
+    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
+        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
+    for label ,color in zip( np.unique(y),colors):
+        position=y==label
+        ax.scatter(X_r[position],np.zeros_like(X_r[position]),
+        label="target= %d"%label,color=color)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.legend(loc="best")
+    ax.set_title("k=%d"%k)
+plt.suptitle("LocallyLinearEmbedding")
+plt.show()
 
