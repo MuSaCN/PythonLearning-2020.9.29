e8f5f13517d8308260f26886a46baa361e34343e
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-01-24 20:49:06 +0800

    update

--- Python大战机器学习/chapter5.py
+++ Python大战机器学习/chapter5.py
@@ -9,7 +9,7 @@ import statsmodels.api as sm
 from scipy import stats
 
 #------------------------------------------------------------
-__mypath__ = MyPath.MyClass_Path("\\Python大战机器学习")  # 路径类
+__mypath__ = MyPath.MyClass_Path()  # 路径类
 myfile = MyFile.MyClass_File()  # 文件操作类
 mytime = MyTime.MyClass_Time()  # 时间类
 myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
@@ -31,7 +31,7 @@ myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
 myML = MyMachineLearning.MyClass_MachineLearning()  # 机器学习综合类
 #------------------------------------------------------------
 
-# ---PCA
+# ---主成分分析 PCA
 from sklearn import decomposition
 iris = myML.DataPre.load_datasets("iris") # 使用 scikit-learn 自带的 iris 数据集
 X,y = iris.data,iris.target
@@ -40,6 +40,7 @@ X,y = iris.data,iris.target
 pca=decomposition.PCA(n_components=None) # 使用默认的 n_components
 pca.fit(X)
 print('explained variance ratio : %s'% str(pca.explained_variance_ratio_))
+
 # 绘制经过 PCA 降维到二维之后的样本点
 myML.plotML.PlotParam_DimRedu(X,y,"decomposition.PCA()",n_components=[2])
 
@@ -57,101 +58,25 @@ X,y = iris.data,iris.target
 # 测试 KernelPCA 的用法
 kernels=['linear','poly','rbf']
 for kernel in kernels:
-    kpca=decomposition.KernelPCA(n_components=None,kernel=kernel) # 依次测试四种核函数
-    kpca.fit(X)
+    kpca=decomposition.KernelPCA(n_components=None,kernel=kernel).fit(X) # 依次测试四种核函数
     print('kernel=%s --> lambdas: %s'% (kernel, kpca.lambdas_) )
 
 # 绘制经过 KernelPCA 降维到二维之后的样本点
-kernels=['linear','poly','rbf','sigmoid']
-fig=plt.figure()
-for i,kernel in enumerate(kernels):
-    kpca=decomposition.KernelPCA(n_components=2,kernel=kernel)
-    kpca.fit(X)
-    X_r=kpca.transform(X)# 原始数据集转换到二维
-    ax=fig.add_subplot(2,2,i+1) ## 两行两列，每个单元显示一种核函数的 KernelPCA 的效果图
-    for label in  np.unique(y):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
-    ax.set_xlabel("X[0]")
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title("kernel=%s"%kernel)
-plt.suptitle("KPCA")
-plt.show()
+kernels=["linear","poly","rbf","sigmoid"]
+myML.plotML.PlotParam_DimRedu(X,y,"decomposition.KernelPCA()",n_components=[2],kernel=kernels)
 
 # 绘制经过 使用 poly 核的KernelPCA 降维到二维之后的样本点
-fig=plt.figure()
-# 每个元素是个元组，代表一组参数（依次为：p 值， gamma 值， r 值）
-# p 取值为：3，10
-# gamma 取值为 ：1，10
-# r 取值为：1，10
-# 排列组合一共 8 种组合
-import itertools
 p=[3,10]; gamma = [1,10] ; r = [1,10]
-Params = list(itertools.product(p,gamma,r))# poly 核的参数组成的列表。
-
-for i,(p,gamma,r) in enumerate(Params):
-    kpca=decomposition.KernelPCA(gamma=gamma,degree=p,coef0=r)  # poly 核，目标为2维
-    kpca.fit(X)
-    X_r=kpca.transform(X)# 原始数据集转换到二维
-    ax=fig.add_subplot(2,4,i+1)## 两行四列，每个单元显示核函数为 poly 的 KernelPCA 一组参数的效果图
-    for label in np.unique(y):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
-    ax.set_xlabel("X[0]")
-    ax.set_xticks([]) # 隐藏 x 轴刻度
-    ax.set_yticks([]) # 隐藏 y 轴刻度
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title(r"$ (%s (x \cdot z+1)+%s)^{%s}$"%(gamma,r,p))
-plt.suptitle("KPCA-Poly")
-plt.show()
-
+myML.plotML.PlotParam_DimRedu(X,y,"decomposition.KernelPCA()",gamma=gamma,degree=p,coef0=r)
 
 # 绘制经过 使用 rbf 核的KernelPCA 降维到二维之后的样本点
-fig=plt.figure()
 Gammas=[0.5,1,4,10]# rbf 核的参数组成的列表。每个参数就是 gamma值
-for i,gamma in enumerate(Gammas):
-    kpca=decomposition.KernelPCA(n_components=2,kernel='rbf',gamma=gamma)
-    kpca.fit(X)
-    X_r=kpca.transform(X)# 原始数据集转换到二维
-    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示核函数为 rbf 的 KernelPCA 一组参数的效果图
-    for label in  np.unique(y):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
-    ax.set_xlabel("X[0]")
-    ax.set_xticks([]) # 隐藏 x 轴刻度
-    ax.set_yticks([]) # 隐藏 y 轴刻度
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title(r"$\exp(-%s||x-z||^2)$"%gamma)
-plt.suptitle("KPCA-rbf")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"decomposition.KernelPCA()",n_components=[2],kernel=['rbf'],gamma=Gammas)
 
-#  绘制经过 使用 sigmoid 核的KernelPCA 降维到二维之后的样本点
-
-fig=plt.figure()
-Params=[(0.01,0.1),(0.01,0.2),(0.1,0.1),(0.1,0.2),(0.2,0.1),(0.2,0.2)]# sigmoid 核的参数组成的列表。
-# 每个元素就是一种参数组合（依次为 gamma,coef0）
-# gamma 取值为： 0.01，0.1，0.2
-# coef0 取值为： 0.1,0.2
-# 排列组合一共有 6 种组合
-for i,(gamma,r) in enumerate(Params):
-    kpca=decomposition.KernelPCA(n_components=2,kernel='sigmoid',gamma=gamma,coef0=r)
-    kpca.fit(X)
-    X_r=kpca.transform(X)# 原始数据集转换到二维
-    ax=fig.add_subplot(3,2,i+1)## 三行两列，每个单元显示核函数为 sigmoid 的 KernelPCA 一组参数的效果图
-    for label  in np.unique(y):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
-    ax.set_xlabel("X[0]")
-    ax.set_xticks([]) # 隐藏 x 轴刻度
-    ax.set_yticks([]) # 隐藏 y 轴刻度
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title(r"$\tanh(%s(x\cdot z)+%s)$"%(gamma,r))
-plt.suptitle("KPCA-sigmoid")
-plt.show()
+# 绘制经过 使用 sigmoid 核的KernelPCA 降维到二维之后的样本点
+gamma = [0.01,0.1,0.2];
+r =[0.1,0.2]
+myML.plotML.PlotParam_DimRedu(X,y,"decomposition.KernelPCA()",n_components=[2],kernel=['sigmoid'],gamma=gamma,coef0=r)
 
 
 # ---MDS
@@ -166,19 +91,7 @@ for n in [4,3,2,1]: # 依次考察降维目标为 4维、3维、2维、1维
     print('stress(n_components=%d) : %s'% (n, str(mds.stress_)))
 
 # 绘制经过 使用 MDS 降维到二维之后的样本点
-mds=manifold.MDS(n_components=2)
-X_r=mds.fit_transform(X) #原始数据集转换到二维
-# 绘制二维图形
-fig=plt.figure()
-ax=fig.add_subplot(1,1,1)
-for label in np.unique(y):
-    position=y==label
-    ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"%label)
-ax.set_xlabel("X[0]")
-ax.set_ylabel("X[1]")
-ax.legend(loc="best")
-ax.set_title("MDS")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"manifold.MDS()",n_components=[2])
 
 
 # ---Isomap
@@ -194,43 +107,11 @@ for n in [4,3,2,1]: # 依次考察降维目标为 4维、3维、2维、1维
 
 # 测试 Isomap 中 n_neighbors 参数的影响，其中降维至 2维
 Ks=[1,5,25,y.size-1] # n_neighbors参数的候选值的集合
-fig=plt.figure()
-for i, k in enumerate(Ks):
-    isomap=manifold.Isomap(n_components=2,n_neighbors=k)
-    X_r=isomap.fit_transform(X)#原始数据集转换到二维
-    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 Isomap 的效果图
-    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
-        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
-    for label ,color in zip( np.unique(y),colors):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"
-        %label,color=color)
-    ax.set_xlabel("X[0]")
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title("k=%d"%k)
-plt.suptitle("Isomap")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"manifold.Isomap()",n_components=[2],n_neighbors=Ks)
 
 # 测试 Isomap 中 n_neighbors 参数的影响，其中降维至 1维
 Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
-fig=plt.figure()
-for i, k in enumerate(Ks):
-    isomap=manifold.Isomap(n_components=1,n_neighbors=k)
-    X_r=isomap.fit_transform(X)#原始数据集转换到 1 维
-    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 Isomap 的效果图
-    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
-        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
-    for label ,color in zip( np.unique(y),colors):
-        position=y==label
-        ax.scatter(X_r[position],np.zeros_like(X_r[position]),
-        label="target= %d"%label,color=color)
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.legend(loc="best")
-    ax.set_title("k=%d"%k)
-plt.suptitle("Isomap")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"manifold.Isomap()",n_components=[1],n_neighbors=Ks)
 
 
 # ---LLE
@@ -246,41 +127,11 @@ for n in [4,3,2,1]:# 依次考察降维目标为 4维、3维、2维、1维
 
 # 测试 LocallyLinearEmbedding 中 n_neighbors 参数的影响，其中降维至 2维
 Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
-fig=plt.figure()
-for i, k in enumerate(Ks):
-    lle=manifold.LocallyLinearEmbedding(n_components=2,n_neighbors=k)
-    X_r=lle.fit_transform(X)#原始数据集转换到二维
-    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 LocallyLinearEmbedding 的效果图
-    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
-        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
-    for label ,color in zip( np.unique(y),colors):
-        position=y==label
-        ax.scatter(X_r[position,0],X_r[position,1],label="target= %d"
-        %label,color=color)
-    ax.set_xlabel("X[0]")
-    ax.set_ylabel("X[1]")
-    ax.legend(loc="best")
-    ax.set_title("k=%d"%k)
-plt.suptitle("LocallyLinearEmbedding")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"manifold.LocallyLinearEmbedding()",n_components=[2],n_neighbors=Ks)
 
 # 测试 LocallyLinearEmbedding 中 n_neighbors 参数的影响，其中降维至 1维
 Ks=[1,5,25,y.size-1]# n_neighbors参数的候选值的集合
-fig=plt.figure()
-for i, k in enumerate(Ks):
-    lle=manifold.LocallyLinearEmbedding(n_components=1,n_neighbors=k)
-    X_r=lle.fit_transform(X)#原始数据集转换到 1 维
-    ax=fig.add_subplot(2,2,i+1)## 两行两列，每个单元显示不同 n_neighbors 参数的 LocallyLinearEmbedding 的效果图
-    colors=((1,0,0),(0,1,0),(0,0,1),(0.5,0.5,0),(0,0.5,0.5),(0.5,0,0.5),
-        (0.4,0.6,0),(0.6,0.4,0),(0,0.6,0.4),(0.5,0.3,0.2),)# 颜色集合，不同标记的样本染不同的颜色
-    for label ,color in zip( np.unique(y),colors):
-        position=y==label
-        ax.scatter(X_r[position],np.zeros_like(X_r[position]),
-        label="target= %d"%label,color=color)
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.legend(loc="best")
-    ax.set_title("k=%d"%k)
-plt.suptitle("LocallyLinearEmbedding")
-plt.show()
+myML.plotML.PlotParam_DimRedu(X,y,"manifold.LocallyLinearEmbedding()",n_components=[1],n_neighbors=Ks)
+
+
 
