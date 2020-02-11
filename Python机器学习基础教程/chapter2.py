daeea4f2613e7eb46a10498cd6e4c46e93a1eda3
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-02-11 17:26:51 +0800

    update

--- Python机器学习基础教程/chapter2.py
+++ Python机器学习基础教程/chapter2.py
@@ -488,31 +488,19 @@ plt.legend()
 # %%
 from MyPackage.bookcode.preamble import *
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_moons
 
-X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,
-                                                    random_state=42)
+X, y = myML.DataPre.make_datasets("moons", n_samples=100, noise=0.25, random_state=3)
+X_train, X_test, y_train, y_test = myML.DataPre.train_test_split(X, y, stratify=y, random_state=42)
 
 forest = RandomForestClassifier(n_estimators=5, random_state=2)
 forest.fit(X_train, y_train)
 
 # %%
-
-fig, axes = plt.subplots(2, 3, figsize=(20, 10))
-for i, (ax, tree) in enumerate(zip(axes.ravel(), forest.estimators_)):
-    ax.set_title("Tree {}".format(i))
-    mglearn.plots.plot_tree_partition(X_train, y_train, tree, ax=ax)
-
-mglearn.plots.plot_2d_separator(forest, X_train, fill=True, ax=axes[-1, -1],
-                                alpha=.4)
-axes[-1, -1].set_title("Random Forest")
-mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+myML.TreeModel.plot_forest_boundary(forest, X_train,y_train,ax=None)
 
 # %%
 
-X_train, X_test, y_train, y_test = train_test_split(
-    cancer.data, cancer.target, random_state=0)
+X_train, X_test, y_train, y_test = myML.DataPre.train_test_split(cancer.data, cancer.target, random_state=0)
 forest = RandomForestClassifier(n_estimators=100, random_state=0)
 forest.fit(X_train, y_train)
 
@@ -520,23 +508,20 @@ print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
 print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
 
 # %%
+myML.TreeModel.plot_feature_importances(forest)
 
-plot_feature_importances_cancer(forest)
 
 # %% md
 
 ###### Strengths, weaknesses, and parameters
 
 # %% md
-
 #### Gradient Boosted Regression Trees (Gradient Boosting Machines)
 
 # %%
-
 from sklearn.ensemble import GradientBoostingClassifier
-
-X_train, X_test, y_train, y_test = train_test_split(
-    cancer.data, cancer.target, random_state=0)
+cancer = myML.DataPre.load_datasets("breast_cancer")
+X_train, X_test, y_train, y_test = myML.DataPre.train_test_split(cancer.data, cancer.target, random_state=0)
 
 gbrt = GradientBoostingClassifier(random_state=0)
 gbrt.fit(X_train, y_train)
@@ -545,10 +530,8 @@ print("Accuracy on training set: {:.3f}".format(gbrt.score(X_train, y_train)))
 print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
 
 # %%
-
 gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
 gbrt.fit(X_train, y_train)
-
 print("Accuracy on training set: {:.3f}".format(gbrt.score(X_train, y_train)))
 print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
 
@@ -564,17 +547,18 @@ print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
 
 gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
 gbrt.fit(X_train, y_train)
+myML.TreeModel.plot_feature_importances(gbrt)
 
-plot_feature_importances_cancer(gbrt)
 
 # %% md
 
 ##### Strengths, weaknesses and parameters
 
 # %% md
-
 #### Kernelized Support Vector Machines
 #### Linear Models and Non-linear Features
+dsafsadfdsafsadfdsadfsadfssdfdfsaa
+from MyPackage.bookcode.preamble import *
 
 # %%
 
