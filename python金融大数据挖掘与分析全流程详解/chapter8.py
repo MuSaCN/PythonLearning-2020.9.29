0128bb08dabf33957d17fb76d51374dbc64f96f2
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-03-08 14:13:55 +0800

    update

--- python金融大数据挖掘与分析全流程详解/chapter8.py
+++ python金融大数据挖掘与分析全流程详解/chapter8.py
@@ -41,47 +41,28 @@ myWebS.__init__(defaultbrowser=True)
 myWebS.get("https://www.baidu.com/")
 myWebS.quit()
 
-# 2.xpath方法来定位元素
-from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get("https://www.baidu.com/")
-browser.find_element_by_xpath('//*[@id="kw"]').send_keys('python')
-browser.find_element_by_xpath('//*[@id="su"]').click()
-
-# 3.css_selector方法来定位元素
-from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get("https://www.baidu.com/")
-browser.find_element_by_css_selector('#kw').send_keys('python')
-browser.find_element_by_css_selector('#su').click()
-
-# 4.browser.page_source方法来获取模拟键盘鼠标点击，百度搜索python后的网页源代码
-from selenium import webdriver
-import time
-browser = webdriver.Chrome()
-browser.get("https://www.baidu.com/")
-browser.find_element_by_xpath('//*[@id="kw"]').send_keys('python')
-browser.find_element_by_xpath('//*[@id="su"]').click()
-time.sleep(3)  # 因为是点击按钮后跳转，所以最好休息3秒钟再进行源代码获取,如果是直接访问网站，则通常不需要等待。
-data = browser.page_source
-print(data)
+# 2.xpath方法 / css_selector方法 来定位元素
+myWebS.__init__(defaultbrowser=True)
+myWebS.get("https://www.baidu.com/")
+ele = myWebS.find_element('//*[@id="kw"]')
+ele.send_keys('python')
+myWebS.find_element('#su').click()
+data = myWebS.page_source(sleep=3)
+myWebS.quit()
 
 # 5.browser.page_source方法来获取新浪财经股票信息
-from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get("http://finance.sina.com.cn/realstock/company/sh000001/nc.shtml")
-data = browser.page_source
+myWebS.__init__(openChrome=True)
+myWebS.get("http://finance.sina.com.cn/realstock/company/sh000001/nc.shtml")
+data = myWebS.page_source()
 print(data)
+myWebS.quit()
 
 # 6.Chrome Headless无界面浏览器设置
-from selenium import webdriver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-browser = webdriver.Chrome(chrome_options=chrome_options)
-browser.get("http://finance.sina.com.cn/realstock/company/sh000001/nc.shtml")
-data = browser.page_source
+myWebS.__init__(openChrome=True,headless=True)
+myWebS.get("http://finance.sina.com.cn/realstock/company/sh000001/nc.shtml")
+data = myWebS.page_source()
 print(data)
-
+myWebS.quit()
 
 
 
