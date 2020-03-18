67a563cd8a43a1bd8df93e3e298ce09bd200927b
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-03-18 16:36:17 +0800

    update

---
+++ My_Python_Items/LibCheck.py
@@ -0,0 +1,27 @@
+# Author:Zhang Yuan
+""
+# ipython或cmd可直接运行
+# 检查安装哪些第三方库和版本
+pip list
+# 生成到指定目录
+pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+# 根据指定目录安装第三方库
+pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+
+# ---cmd方式自动执行
+# 批量生成
+'''
+@echo off
+pip list
+pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+taskkill /f /im cmd.exe
+exit
+'''
+# 批量安装
+'''
+@echo off
+pip list
+pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+taskkill /f /im cmd.exe
+exit
+'''
