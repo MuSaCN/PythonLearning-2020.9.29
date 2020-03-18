73837403b6e11ff9ccb3914a3c80d7463427a834
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-03-18 16:39:00 +0800

    update

--- My_Python_Items/LibCheck.py
+++ My_Python_Items/LibCheck.py
@@ -2,18 +2,18 @@
 ""
 # ipython或cmd可直接运行
 # 检查安装哪些第三方库和版本
-pip list
+'pip list'
 # 生成到指定目录
-pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+'pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages_record.txt"'
 # 根据指定目录安装第三方库
-pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+'pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages_record.txt"'
 
 # ---cmd方式自动执行
 # 批量生成
 '''
 @echo off
 pip list
-pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+pip freeze > "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages_record.txt"
 taskkill /f /im cmd.exe
 exit
 '''
@@ -21,7 +21,7 @@ exit
 '''
 @echo off
 pip list
-pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages.txt"
+pip install -r "C:\\Users\\i2011\\OneDrive\\Files_Document\\site-packages_record.txt"
 taskkill /f /im cmd.exe
 exit
 '''
