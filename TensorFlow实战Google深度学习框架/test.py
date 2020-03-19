be8c93355de393d83a46ebade2253810ba817fcd
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-03-19 12:02:00 +0800

    update

--- TensorFlow实战Google深度学习框架/test.py
+++ TensorFlow实战Google深度学习框架/test.py
@@ -44,9 +44,116 @@ myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
 import tensorflow as tf
 a = tf.constant([1,2])
 b = tf.constant([3,4])
-a+b
+result = a+b
+sess = tf.compat.v1.Session()
+sess.run(result)
 
+#### 1. 定义两个不同的图
+import tensorflow as tf
 
+g1 = tf.Graph()
+with g1.as_default():
+    v = tf.compat.v1.get_variable("v", [1], initializer=tf.zeros_initializer())  # 设置初始值为0
 
+g2 = tf.Graph()
+with g2.as_default():
+    v = tf.compat.v1.get_variable("v", [1], initializer=tf.ones_initializer())  # 设置初始值为1
 
+with tf.compat.v1.Session(graph=g1) as sess:
+    tf.compat.v1.global_variables_initializer().run()
+    with tf.compat.v1.variable_scope("", reuse=True):
+        print(sess.run(tf.compat.v1.get_variable("v")))
+
+with tf.Session(graph=g2) as sess:
+    tf.global_variables_initializer().run()
+    with tf.variable_scope("", reuse=True):
+        print(sess.run(tf.get_variable("v")))
+
+# %% md
+
+#### 2. 张量的概念
+
+# %%
+
+import tensorflow as tf
+
+a = tf.constant([1.0, 2.0], name="a")
+b = tf.constant([2.0, 3.0], name="b")
+result = a + b
+print(result)
+
+sess = tf.InteractiveSession()
+print(result.eval())
+sess.close()
+
+# %% md
+
+#### 3. 会话的使用
+
+# %% md
+
+3.1
+创建和关闭会话
+
+# %%
+
+# 创建一个会话。
+sess = tf.Session()
+
+# 使用会话得到之前计算的结果。
+print(sess.run(result))
+
+# 关闭会话使得本次运行中使用到的资源可以被释放。
+sess.close()
+
+# %% md
+
+3.2
+使用with
+statement
+来创建会话
+
+# %%
+
+with tf.Session() as sess:
+    print(sess.run(result))
+
+# %% md
+
+3.3
+指定默认会话
+
+# %%
+
+sess = tf.Session()
+with sess.as_default():
+    print(result.eval())
+
+# %%
+
+sess = tf.Session()
+
+# 下面的两个命令有相同的功能。
+print(sess.run(result))
+print(result.eval(session=sess))
+
+# %% md
+
+#### 4. 使用tf.InteractiveSession构建会话
+
+# %%
+
+sess = tf.InteractiveSession()
+print(result.eval())
+sess.close()
+
+# %% md
+
+#### 5. 通过ConfigProto配置会话
+
+# %%
+
+config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+sess1 = tf.InteractiveSession(config=config)
+sess2 = tf.Session(config=config)
 
