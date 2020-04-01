3a123f11a1af7ac354a5498b2823a7b1262469e8
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-04-01 12:41:05 +0800

    update

--- DeepLearningWithPython/3.5.py
+++ DeepLearningWithPython/3.5.py
@@ -41,8 +41,7 @@ myReportA = MyQuant.MyClass_ReportAnalysis()  # 研报分析类
 myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
 #------------------------------------------------------------
 
-
-
+#%%
 import keras
 keras.__version__
 
@@ -59,7 +58,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 # We decode the review; note that our indices were offset by 3
 # because 0, 1 and 2 are reserved indices for "padding", "start of sequence", and "unknown".
-decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[2]])
 decoded_review
 
 
@@ -68,14 +67,17 @@ def vectorize_sequences(sequences, dimension=10000):
     # Create an all-zero matrix of shape (len(sequences), dimension)
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
+        # 批量赋值
         results[i, sequence] = 1.  # set specific indices of results[i] to 1s
     return results
+
 # Our vectorized training data
 x_train = vectorize_sequences(train_data)
+train_data.shape
+x_train.shape
+
 # Our vectorized test data
 x_test = vectorize_sequences(test_data)
-
-x_train[0]
 
 # Our vectorized labels
 y_train = np.asarray(train_labels).astype('float32')
@@ -90,10 +92,10 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+# 模型编译的几种方式：
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-
 
 from keras import optimizers
 model.compile(optimizer=optimizers.RMSprop(lr=0.001),
@@ -102,7 +104,6 @@ model.compile(optimizer=optimizers.RMSprop(lr=0.001),
 
 from keras import losses
 from keras import metrics
-
 model.compile(optimizer=optimizers.RMSprop(lr=0.001),
               loss=losses.binary_crossentropy,
               metrics=[metrics.binary_accuracy])
@@ -114,7 +115,7 @@ partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
-
+# validation_data指定验证集
 history = model.fit(partial_x_train,
                     partial_y_train,
                     epochs=20,
@@ -125,7 +126,7 @@ history_dict = history.history
 history_dict.keys()
 
 
-myplt.set_backend("tkagg")
+
 import matplotlib.pyplot as plt
 
 acc = history.history['binary_accuracy']
@@ -134,7 +135,8 @@ loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
-
+#%%
+myplt.set_backend("TKAgg")
 # "bo" is for "blue dot"
 plt.plot(epochs, loss, 'bo', label='Training loss')
 # b is for "solid blue line"
@@ -145,7 +147,7 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-
+#%%
 plt.clf()   # clear figure
 acc_values = history_dict['binary_accuracy']
 val_acc_values = history_dict['val_binary_accuracy']
@@ -159,6 +161,7 @@ plt.legend()
 
 plt.show()
 
+#%%
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
@@ -170,153 +173,8 @@ model.compile(optimizer='rmsprop',
 
 model.fit(x_train, y_train, epochs=4, batch_size=512)
 results = model.evaluate(x_test, y_test)
-
-
 results
 model.predict(x_test)
-
-
-#%%
-import keras
-keras.__version__
-from keras.datasets import boston_housing
-(train_data, train_targets), (test_data, test_targets) =  boston_housing.load_data()
-train_data.shape
-test_data.shape
-train_targets
-
-mean = train_data.mean(axis=0)
-train_data -= mean
-std = train_data.std(axis=0)
-train_data /= std
-test_data -= mean
-test_data /= std
-
-
-from keras import models
-from keras import layers
-
-def build_model():
-    # Because we will need to instantiate
-    # the same model multiple times,
-    # we use a function to construct it.
-    model = models.Sequential()
-    model.add(layers.Dense(64, activation='relu',
-                           input_shape=(train_data.shape[1],)))
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(1))
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-    return model
-
-#%%
-import numpy as np
-
-k = 4
-num_val_samples = len(train_data) // k
-num_epochs = 100
-all_scores = []
-for i in range(k):
-    print('processing fold #', i)
-    # Prepare the validation data: data from partition # k
-    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-    # Prepare the training data: data from all other partitions
-    partial_train_data = np.concatenate(
-        [train_data[:i * num_val_samples],
-         train_data[(i + 1) * num_val_samples:]],
-        axis=0)
-    partial_train_targets = np.concatenate(
-        [train_targets[:i * num_val_samples],
-         train_targets[(i + 1) * num_val_samples:]],
-        axis=0)
-    # Build the Keras model (already compiled)
-    model = build_model()
-    # Train the model (in silent mode, verbose=0)
-    model.fit(partial_train_data, partial_train_targets,
-              epochs=num_epochs, batch_size=1, verbose=0)
-    # Evaluate the model on the validation data
-    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-    all_scores.append(val_mae)
-
-all_scores
-np.mean(all_scores)
-
-#%%
-from keras import backend as K
-
-# Some memory clean-up
-K.clear_session()
-
-num_epochs = 500
-all_mae_histories = []
-for i in range(k):
-    print('processing fold #', i)
-    # Prepare the validation data: data from partition # k
-    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-
-    # Prepare the training data: data from all other partitions
-    partial_train_data = np.concatenate(
-        [train_data[:i * num_val_samples],
-         train_data[(i + 1) * num_val_samples:]],
-        axis=0)
-    partial_train_targets = np.concatenate(
-        [train_targets[:i * num_val_samples],
-         train_targets[(i + 1) * num_val_samples:]],
-        axis=0)
-
-    # Build the Keras model (already compiled)
-    model = build_model()
-    # Train the model (in silent mode, verbose=0)
-    history = model.fit(partial_train_data, partial_train_targets,
-                        validation_data=(val_data, val_targets),
-                        epochs=num_epochs, batch_size=1, verbose=0)
-    mae_history = history.history['val_mean_absolute_error']
-    all_mae_histories.append(mae_history)
-
-
-average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
-
-#%%
-
-import matplotlib.pyplot as plt
-
-plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
-plt.xlabel('Epochs')
-plt.ylabel('Validation MAE')
-plt.show()
-
-#%%
-
-def smooth_curve(points, factor=0.9):
-  smoothed_points = []
-  for point in points:
-    if smoothed_points:
-      previous = smoothed_points[-1]
-      smoothed_points.append(previous * factor + point * (1 - factor))
-    else:
-      smoothed_points.append(point)
-  return smoothed_points
-
-smooth_mae_history = smooth_curve(average_mae_history[10:])
-
-plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
-plt.xlabel('Epochs')
-plt.ylabel('Validation MAE')
-plt.show()
-
-#%%
-
-# Get a fresh, compiled model.
-model = build_model()
-# Train it on the entirety of the data.
-model.fit(train_data, train_targets,
-          epochs=80, batch_size=16, verbose=0)
-test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
-
-#%%
-
-test_mae_score
 
 
 
