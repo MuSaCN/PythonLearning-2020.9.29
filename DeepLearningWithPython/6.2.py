ed76cf09cd2558b38642028090c06be768d697e4
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-04-07 18:02:10 +0800

    update

--- DeepLearningWithPython/6.2.py
+++ DeepLearningWithPython/6.2.py
@@ -87,10 +87,11 @@ print('Loading data...')
 (input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
 print(len(input_train), 'train sequences')
 print(len(input_test), 'test sequences')
+np.array(input_train[0]).shape
 
 print('Pad sequences (samples x time)')
 # pad_sequences 将多个序列截断或补齐为相同长度。
-input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+input_train = sequence.pad_sequences(input_train, maxlen=maxlen) # 原长218，先长 maxlen
 input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
 print('input_train shape:', input_train.shape)
 print('input_test shape:', input_test.shape)
@@ -107,7 +108,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 history = model.fit(input_train, y_train,
                     epochs=10,
-                    batch_size=128,
+                    batch_size=512,
                     validation_split=0.2)
 
 #%%
