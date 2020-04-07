deb5c83ffa43c4a7407ae6729eda0f856c984bad
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-04-07 14:28:14 +0800

    update

---
+++ DeepLearningWithPython/6.2.py
@@ -0,0 +1,136 @@
+# Author:Zhang Yuan
+from MyPackage import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+#------------------------------------------------------------
+__mypath__ = MyPath.MyClass_Path("\\DeepLearningWithPython")  # 路径类
+myfile = MyFile.MyClass_File()  # 文件操作类
+myword = MyFile.MyClass_Word()  # word生成类
+myexcel = MyFile.MyClass_Excel()  # excel生成类
+mytime = MyTime.MyClass_Time()  # 时间类
+myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
+mypltpro = MyPlot.MyClass_PlotPro()  # Plot高级图系列
+myfig = MyPlot.MyClass_Figure(AddFigure=False)  # 对象式绘图类(可多个图窗)
+myfigpro = MyPlot.MyClass_FigurePro(AddFigure=False)  # Figure高级图系列
+mynp = MyArray.MyClass_NumPy()  # 多维数组类(整合Numpy)
+mypd = MyArray.MyClass_Pandas()  # 矩阵数组类(整合Pandas)
+mypdpro = MyArray.MyClass_PandasPro()  # 高级矩阵数组类
+myDA = MyDataAnalysis.MyClass_DataAnalysis()  # 数据分析类
+# myMql = MyMql.MyClass_MqlBackups() # Mql备份类
+# myMT5 = MyMql.MyClass_ConnectMT5(connect=False) # Python链接MetaTrader5客户端类
+# myDefault = MyDefault.MyClass_Default_Matplotlib() # matplotlib默认设置
+# myBaidu = MyWebCrawler.MyClass_BaiduPan() # Baidu网盘交互类
+# myImage = MyImage.MyClass_ImageProcess()  # 图片处理类
+myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
+myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
+myML = MyMachineLearning.MyClass_MachineLearning()  # 机器学习综合类
+mySQL = MyDatabase.MyClass_MySQL(connect=False)  # MySQL类
+mySQLAPP = MyDatabase.MyClass_SQL_APPIntegration()  # 数据库应用整合
+myWebQD = MyWebCrawler.MyClass_QuotesDownload(tushare=False)  # 金融行情下载类
+myWebR = MyWebCrawler.MyClass_Requests()  # Requests爬虫类
+myWebS = MyWebCrawler.MyClass_Selenium(openChrome=False)  # Selenium模拟浏览器类
+myWebAPP = MyWebCrawler.MyClass_Web_APPIntegration()  # 爬虫整合应用类
+myEmail = MyWebCrawler.MyClass_Email()  # 邮箱交互类
+myReportA = MyQuant.MyClass_ReportAnalysis()  # 研报分析类
+myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
+myKeras = MyDeepLearning.MyClass_Keras()  # Keras综合类
+#------------------------------------------------------------
+
+
+
+#%%
+
+import keras
+keras.__version__
+
+#%%
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN
+
+model = Sequential()
+model.add(Embedding(10000, 32))
+model.add(SimpleRNN(32))
+model.summary()
+
+#%%
+
+model = Sequential()
+model.add(Embedding(10000, 32))
+model.add(SimpleRNN(32, return_sequences=True))
+model.summary()
+
+#%%
+model = Sequential()
+model.add(Embedding(10000, 32))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32))  # This last layer only returns the last outputs.
+model.summary()
+
+#%%
+
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 10000  # 作为特征的单词个数
+maxlen = 500  # 在这么多单词之后截断文本（这些单词都属于前 max_features 个最常见的单词）
+batch_size = 32
+
+print('Loading data...')
+(input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+# pad_sequences 将多个序列截断或补齐为相同长度。
+input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
+print('input_train shape:', input_train.shape)
+print('input_test shape:', input_test.shape)
+
+
+#%%
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(input_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+
+#%%
+myKeras.plot_acc_loss(history)
+
+
+#%%
+# 长短期记忆（LSTM，long short-term memory）算法。LSTM单元的作用：允许过去的信息稍后重新进入，从而解决梯度消失问题。
+from keras.layers import LSTM
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(input_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+
+#%%
+myKeras.plot_acc_loss(history)
+
