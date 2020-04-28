c22219a26e1264be98d40e0364e2d8e3ee599740
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-04-28 13:59:21 +0800

    update

--- Hands-On Machine Learning/chapter12.py
+++ Hands-On Machine Learning/chapter12.py
@@ -198,22 +198,14 @@ variance
 
 #%% md
 ## Custom loss function
-
-#%% md
-
-Let's start by loading and preparing the California housing dataset. We first load it, then split it into a training set, a validation set and a test set, and finally we scale it:
-
-#%%
-
+# Let's start by loading and preparing the California housing dataset. We first load it, then split it into a training set, a validation set and a test set, and finally we scale it:
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 housing = fetch_california_housing()
-X_train_full, X_test, y_train_full, y_test = train_test_split(
-    housing.data, housing.target.reshape(-1, 1), random_state=42)
-X_train, X_valid, y_train, y_valid = train_test_split(
-    X_train_full, y_train_full, random_state=42)
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target.reshape(-1, 1), random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
@@ -221,7 +213,7 @@ X_valid_scaled = scaler.transform(X_valid)
 X_test_scaled = scaler.transform(X_test)
 
 #%%
-
+# keras.losses.Huber
 def huber_fn(y_true, y_pred):
     error = y_true - y_pred
     is_small_error = tf.abs(error) < 1
@@ -230,24 +222,6 @@ def huber_fn(y_true, y_pred):
     return tf.where(is_small_error, squared_loss, linear_loss)
 
 #%%
-
-plt.figure(figsize=(8, 3.5))
-z = np.linspace(-4, 4, 200)
-plt.plot(z, huber_fn(0, z), "b-", linewidth=2, label="huber($z$)")
-plt.plot(z, z**2 / 2, "b:", linewidth=1, label=r"$\frac{1}{2}z^2$")
-plt.plot([-1, -1], [0, huber_fn(0., -1.)], "r--")
-plt.plot([1, 1], [0, huber_fn(0., 1.)], "r--")
-plt.gca().axhline(y=0, color='k')
-plt.gca().axvline(x=0, color='k')
-plt.axis([-4, 4, 0, 4])
-plt.grid(True)
-plt.xlabel("$z$")
-plt.legend(fontsize=14)
-plt.title("Huber loss", fontsize=14)
-plt.show()
-
-#%%
-
 input_shape = X_train.shape[1:]
 
 model = keras.models.Sequential([
@@ -272,14 +246,10 @@ model.fit(X_train_scaled, y_train, epochs=2,
 #%%
 
 model.save("my_model_with_a_custom_loss.h5")
-
-#%%
-
 model = keras.models.load_model("my_model_with_a_custom_loss.h5",
                                 custom_objects={"huber_fn": huber_fn})
 
 #%%
-
 model.fit(X_train_scaled, y_train, epochs=2,
           validation_data=(X_valid_scaled, y_valid))
 
@@ -318,7 +288,8 @@ model.fit(X_train_scaled, y_train, epochs=2,
           validation_data=(X_valid_scaled, y_valid))
 
 #%%
-
+import tensorflow as tf
+from tensorflow import keras
 class HuberLoss(keras.losses.Loss):
     def __init__(self, threshold=1.0, **kwargs):
         self.threshold = threshold
@@ -332,6 +303,23 @@ class HuberLoss(keras.losses.Loss):
     def get_config(self):
         base_config = super().get_config()
         return {**base_config, "threshold": self.threshold}
+    def plot_huber_loss(self):
+        plt.figure(figsize=(8, 3.5))
+        z = np.linspace(-4, 4, 200)
+        plt.plot(z, self.call(0, z), "b-", linewidth=2, label="huber($z$)")
+        plt.plot(z, z ** 2 / 2, "b:", linewidth=1, label=r"$\frac{1}{2}z^2$")
+        plt.plot([-1, -1], [0, self.call(0., -1.)], "r--")
+        plt.plot([1, 1], [0, self.call(0., 1.)], "r--")
+        plt.gca().axhline(y=0, color='k')
+        plt.gca().axvline(x=0, color='k')
+        plt.axis([-4, 4, 0, 4])
+        plt.grid(True)
+        plt.xlabel("$z$")
+        plt.legend(fontsize=14)
+        plt.title("Huber loss", fontsize=14)
+        plt.show()
+a= HuberLoss()
+a.plot_huber_loss()
 
 #%%
 
