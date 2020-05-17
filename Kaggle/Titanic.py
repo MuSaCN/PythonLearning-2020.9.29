99ef7a86bc3a5fbb534551d00b80adce05706718
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-05-17 13:26:12 +0800

    update

--- Kaggle/Titanic.py
+++ Kaggle/Titanic.py
@@ -43,6 +43,38 @@ myKeras = MyDeepLearning.MyClass_tfKeras()  # tfKeras综合类
 myTensor = MyDeepLearning.MyClass_TensorFlow()  # Tensorflow综合类
 #------------------------------------------------------------
 
+path = r"C:\Users\i2011\.kaggle\titanic"
+
+train_data = pd.read_csv(path+"\\train.csv")
+train_data.head()
+
+test_data = pd.read_csv(path+"\\test.csv")
+test_data.head()
+
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+print("% of women who survived:", rate_women)
+
+men = train_data.loc[train_data.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+print("% of men who survived:", rate_men)
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
 
 
 
