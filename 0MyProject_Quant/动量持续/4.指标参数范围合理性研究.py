bf3704282cde2832d4cbf3ac8418a5f437e3a6d3
MuSaCN <435116098@qq.com>
2020-08-06 10:31:19 +0800

    update

---
+++ 0MyProject_Quant/动量持续/4.指标参数范围合理性研究.py
@@ -0,0 +1,62 @@
+# Author:Zhang Yuan
+from MyPackage import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+#------------------------------------------------------------
+__mypath__ = MyPath.MyClass_Path("")  # 路径类
+myfile = MyFile.MyClass_File()  # 文件操作类
+myword = MyFile.MyClass_Word()  # word生成类
+myexcel = MyFile.MyClass_Excel()  # excel生成类
+mytime = MyTime.MyClass_Time()  # 时间类
+myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
+mypltpro = MyPlot.MyClass_PlotPro()  # Plot高级图系列
+myfig = MyPlot.MyClass_Figure(AddFigure=False)  # 对象式绘图类(可多个图窗)
+myfigpro = MyPlot.MyClass_FigurePro(AddFigure=False)  # Figure高级图系列
+mynp = MyArray.MyClass_NumPy()  # 多维数组类(整合Numpy)
+mypd = MyArray.MyClass_Pandas()  # 矩阵数组类(整合Pandas)
+mypdpro = MyArray.MyClass_PandasPro()  # 高级矩阵数组类
+myDA = MyDataAnalysis.MyClass_DataAnalysis()  # 数据分析类
+myDefault = MyDefault.MyClass_Default_Matplotlib()  # 画图恢复默认设置类
+# myMql = MyMql.MyClass_MqlBackups() # Mql备份类
+# myBaidu = MyWebCrawler.MyClass_BaiduPan() # Baidu网盘交互类
+# myImage = MyImage.MyClass_ImageProcess()  # 图片处理类
+myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
+myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
+myML = MyMachineLearning.MyClass_MachineLearning()  # 机器学习综合类
+mySQL = MyDataBase.MyClass_MySQL(connect=False)  # MySQL类
+mySQLAPP = MyDataBase.MyClass_SQL_APPIntegration()  # 数据库应用整合
+myWebQD = MyWebCrawler.MyClass_QuotesDownload(tushare=False)  # 金融行情下载类
+myWebR = MyWebCrawler.MyClass_Requests()  # Requests爬虫类
+myWebS = MyWebCrawler.MyClass_Selenium(openChrome=False)  # Selenium模拟浏览器类
+myWebAPP = MyWebCrawler.MyClass_Web_APPIntegration()  # 爬虫整合应用类
+myEmail = MyWebCrawler.MyClass_Email()  # 邮箱交互类
+myReportA = MyQuant.MyClass_ReportAnalysis()  # 研报分析类
+myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
+myKeras = MyDeepLearning.MyClass_tfKeras()  # tfKeras综合类
+myTensor = MyDeepLearning.MyClass_TensorFlow()  # Tensorflow综合类
+myMT5 = MyMql.MyClass_ConnectMT5(connect=False)  # Python链接MetaTrader5客户端类
+myPjMT5 = MyProject.MT5_MLLearning()  # MT5机器学习项目类
+myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
+#------------------------------------------------------------
+
+'''
+# 研究思路，类似 Permutation Test 置换检验：
+分析 Indicator 与 Return 是否有关，不能直接用简单的相关系数，需要个基准。
+1.用 spearman 相关系数；
+2.一次实验：随机生成一组白噪声(可以为正态分布、t分布)，计算 noise 与 Indicator 的相关系数。
+3.重复许多次实验，得到相关系数序列。这个序列可以形成一个分布。能计算出 0.05 位置 和 0.95 位置的值。
+4.计算某一参数 Indicator 和 Return 的相关系数，并且能得出其在上述分布中的概率。
+5.不同参数下 Indicator 都有一个概率，这就会形成一个序列。通过序列能判断哪些参数区间有效。
+'''
+
+
+
+
+
+
