16a3649bfb7418ff3bcba021ce675ed3a6e0fe74
MuSa <39754824+MuSaCN@users.noreply.github.com>
2020-09-27 18:52:35 +0800

    update

--- 0MyProject_Quant/动量持续/4.动量单独测试(分析).py
+++ 0MyProject_Quant/动量持续/4.动量单独测试(分析).py
@@ -57,8 +57,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # ---获取数据
-symbol = "AUDNZD"
-timeframe = "TIMEFRAME_H12"
+symbol = "AUDUSD"
+timeframe = "TIMEFRAME_D1"
 
 date_from, date_to = myPjMT5.get_date_range(timeframe)
 data_total = myPjMT5.getsymboldata(symbol,timeframe,date_from,date_to,index_time=True, col_capitalize=True)
@@ -71,7 +71,7 @@ train_x1 = data_train.index[-1]
 
 #%%
 # ---仅做多分析
-k_range = [k for k in range(112, 116+1)]
+k_range = [k for k in range(112, 112+1)]
 holding_range = [holding for holding in range(1, 1+1)]
 lag_trade_range = [lag_trade for lag_trade in range(1, 1+1)]
 
@@ -92,7 +92,7 @@ myBTV.signal_quality_explain()
 
 #%%
 # ---仅做空分析
-k_range = [k for k in range(314, 314+1)]
+k_range = [k for k in range(112, 112+1)]
 holding_range = [holding for holding in range(1, 1+1)]
 lag_trade_range = [lag_trade for lag_trade in range(1, 1+1)]
 
